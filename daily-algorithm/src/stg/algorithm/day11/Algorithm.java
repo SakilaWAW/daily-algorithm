6b3aba571e2f5b11bda50794ff53c39dad16e17c
STG <pccp0909@sina.com>
2016-11-10 14:41:53 +0800

    fix bug

--- daily-algorithm/src/stg/algorithm/day11/Algorithm.java
+++ daily-algorithm/src/stg/algorithm/day11/Algorithm.java
@@ -4,7 +4,7 @@ package stg.algorithm.day11;
  * Given a non-empty array of integers, return the third maximum number in this array. If it does not exist, return the maximum number. The time complexity must be in O(n).
  * Example 1:
  * 	Input: [3, 2, 1]
-**	Output: 1
+ *	Output: 1
  *	Explanation: The third maximum is 1.
  *
  *	Example 2:
